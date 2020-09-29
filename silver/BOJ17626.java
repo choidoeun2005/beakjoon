1d6b221e2be6cf8a5a14737bc99fc52bb32c66af
naruseon <naruseon22@gmail.com>
2020-09-29 11:39:23 +0900

    add some files

---
+++ silver/BOJ17626.java
@@ -0,0 +1,28 @@
+package com.github.naruseon.beakjoon.silver;
+
+// silver 5, Four Squares
+
+import java.util.Scanner;
+
+public class BOJ17626 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] DP = new int[N + 1];
+        DP[0] = 0;
+        DP[1] = 1;
+
+        int min;
+
+        for (int i = 2; i <= N; i++) {
+            min = Integer.MAX_VALUE;
+
+            for (int j = 1; j * j <= i; j++) {
+                int temp = i - j * j;
+                min = Math.min(min, DP[temp]);
+            }
+            DP[i] = min + 1;
+        }
+        System.out.println(DP[N]);
+    }
+}
---
+++ silver/BOJ9205.java
@@ -0,0 +1,38 @@
+package com.github.naruseon.beakjoon.silver;
+
+import java.util.Scanner;
+
+// TODO
+
+public class BOJ9205 {
+    public static void main(String[] args) {
+        boolean check = true;
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        int T = sc.nextInt();
+        for (int i = 0; i < T; i++) {
+            int store = sc.nextInt();
+            Pair[] info = new Pair[store + 2];
+            for (int j = 0; j < info.length; j++) {
+                info[j] = new Pair(sc.nextInt(), sc.nextInt());
+            }
+            for (int j = 0; j < store + 1; j++) {
+                if (distance(info[j], info[j + 1]) > 1000) check = false;
+            }
+            if (check) sb.append("happy").append("\n"); else sb.append("sad").append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+
+    public static int distance(Pair x, Pair y) {
+        return Math.abs(x.x - y.x) + Math.abs(x.y - y.y);
+    }
+
+    static class Pair {
+        int x, y;
+        public Pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
