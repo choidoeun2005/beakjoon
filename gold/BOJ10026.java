2eacc917bccd956314be04232c7607ce81781077
naruseon <naruseon22@gmail.com>
2020-09-29 14:23:49 +0900

    add some files

---
+++ gold/BOJ10026.java
@@ -0,0 +1,89 @@
+package com.github.naruseon.beakjoon.gold;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+// gold 5, 적록색약
+
+public class BOJ10026 {
+
+    static int normalcnt = 0;
+    static int colorcnt = 0;
+    static int N;
+    static char[][] grid1;
+    static char[][] grid;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        grid = new char[N][N];
+        grid1 = new char[N][N];
+        for (int i = 0; i < N; i++) {
+            char[] c = sc.next().toCharArray();
+            grid[i] = c.clone();
+            grid1[i] = c.clone();
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (grid[i][j] == 'R')
+                    grid[i][j] = 'G';
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (grid[i][j] != 'X') {
+                    bfs(grid, i, j);
+                    colorcnt++;
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (grid1[i][j] != 'X') {
+                    bfs(grid1, i, j);
+                    normalcnt++;
+                }
+            }
+        }
+
+        System.out.println(normalcnt + " " + colorcnt);
+    }
+
+    public static void bfs(char[][] grid, int x, int y) {
+        Queue<Pair> queue = new LinkedList<>();
+        char std = grid[x][y];
+        grid[x][y] = 'X';
+        queue.offer(new Pair(x, y));
+
+        while (!queue.isEmpty()) {
+            Pair pair = queue.poll();
+            for (int i = 0; i < 4; i++) {
+                int nextX = pair.x + new int[]{1, -1, 0, 0}[i];
+                int nextY = pair.y + new int[]{0, 0, 1, -1}[i];
+                if (nextX < 0 || nextY < 0 || nextX >= N || nextY >= N) {
+                    continue;
+                }
+                if (grid[nextX][nextY] == 'X') {
+                    continue;
+                }
+                if (grid[nextX][nextY] == std) {
+                    queue.offer(new Pair(nextX, nextY));
+                    grid[nextX][nextY] = 'X';
+                }
+            }
+        }
+    }
+
+    static class Pair {
+        int x, y;
+
+        public Pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
---
+++ silver/BOJ5430.java
@@ -0,0 +1,56 @@
+package com.github.naruseon.beakjoon.silver;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+// silver 2, AC
+
+public class BOJ5430 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        Scanner sc = new Scanner(System.in);
+        boolean rotated;
+        int N = sc.nextInt();
+        outer:
+        for (int i = 0; i < N; i++) {
+            rotated = false;
+            char[] cmd = sc.next().toCharArray();
+            int len = sc.nextInt();
+            ArrayList <Integer> arr = new ArrayList<>();
+            String[] info = sc.next().split(",");
+            info[0] = info[0].substring(1);
+            info[info.length - 1] = info[info.length - 1].substring(0, info[info.length - 1].length() - 1);
+            for (String s : info) {
+                if (!s.equals(""))
+                    arr.add(Integer.parseInt(s));
+            }
+
+            for (char c : cmd) {
+                if (c == 'R') {
+                    rotated = !rotated;
+                }
+                if (c == 'D') {
+                    try {
+                        if (rotated) arr.remove(arr.size() - 1);
+                        else arr.remove(0);
+                    } catch (Exception e) {
+                        sb.append("error").append("\n");
+                        continue outer;
+                    }
+                }
+            }
+            if (arr.size() == 0) {
+                sb.append("[]\n");
+                continue;
+            }
+            if (rotated) Collections.reverse(arr);
+            sb.append("[");
+            for (int j = 0; j < arr.size() - 1; j++) {
+                sb.append(arr.get(j)).append(",");
+            }
+            sb.append(arr.get(arr.size() - 1)).append("]").append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+}
