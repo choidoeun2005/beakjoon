0e54cf4c99b020195ec57de2ec9879629aa87408
naruseon <naruseon22@gmail.com>
2020-10-22 23:40:41 +0900

    add some files

--- silver/BOJ2579.java
+++ silver/BOJ2579.java
@@ -2,15 +2,32 @@ package com.github.naruseon.beakjoon.silver;
 
 import java.util.Scanner;
 
-// TODO
+// 계단 오르기, silver 3
 
 public class BOJ2579 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int[] stair = new int[N];
+        int[] DP = new int[N];
         for (int i = 0; i < N; i++) {
             stair[i] = sc.nextInt();
         }
+        DP[0] = stair[0];
+        if (N == 1) {
+            System.out.println(DP[0]);
+            return;
+        }
+        DP[1] = Math.max(stair[0] + stair[1], stair[1]);
+        if (N == 2) {
+            System.out.println(DP[1]);
+            return;
+        }
+        DP[2] = Math.max(stair[0] + stair[2], stair[1] + stair[2]);
+
+        for (int i = 3; i < N; i++) {
+            DP[i] = Math.max(DP[i - 2] + stair[i], DP[i - 3] + stair[i - 1] + stair[i]);
+        }
+        System.out.println(DP[N - 1]);
     }
 }
--- silver/BOJ9205.java
+++ silver/BOJ9205.java
@@ -1,33 +1,50 @@
 package com.github.naruseon.beakjoon.silver;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
-
-// TODO
 
 // 맥주 마시면서 걸어가기, silver 1
 
 public class BOJ9205 {
+    static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) {
-        boolean check = true;
         Scanner sc = new Scanner(System.in);
-        StringBuilder sb = new StringBuilder();
         int T = sc.nextInt();
         for (int i = 0; i < T; i++) {
-            int store = sc.nextInt();
-            Pair[] info = new Pair[store + 2];
-            for (int j = 0; j < info.length; j++) {
-                info[j] = new Pair(sc.nextInt(), sc.nextInt());
+            int CV_num = sc.nextInt();
+            Pair home = new Pair(sc.nextInt(), sc.nextInt());
+            Pair[] CV = new Pair[CV_num];
+            for (int h = 0; h < CV_num; h++) {
+                CV[h] = new Pair(sc.nextInt(), sc.nextInt());
             }
-            for (int j = 0; j < store + 1; j++) {
-                if (distance(info[j], info[j + 1]) > 1000) check = false;
-            }
-            if (check) sb.append("happy").append("\n"); else sb.append("sad").append("\n");
+            Pair des = new Pair(sc.nextInt(), sc.nextInt());
+            bfs(home, CV, des);
         }
         System.out.println(sb.toString());
     }
 
     public static int distance(Pair x, Pair y) {
         return Math.abs(x.x - y.x) + Math.abs(x.y - y.y);
+    }
+
+    public static void bfs(Pair home, Pair[] CV, Pair des) {
+        Queue<Pair> queue = new LinkedList<>();
+        queue.offer(home);
+        while(!queue.isEmpty()) {
+            Pair cur = queue.poll();
+            if (distance(cur, des) <= 1000) {
+                sb.append("happy").append("\n");
+                return;
+            }
+            for (int i = 0; i < CV.length; i++) {
+                if (distance(CV[i], cur) <= 1000) {
+                    queue.offer(CV[i]);
+                    CV[i] = new Pair(10000000, 10000000);
+                }
+            }
+        }
+        sb.append("sad").append("\n");
     }
 
     static class Pair {
