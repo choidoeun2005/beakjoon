ccb4e447c98c20fe5c02b23539a0804aacae0fca
naruseon <71130816+naruseon@users.noreply.github.com>
2020-12-19 11:39:45 +0900

    Add files via upload

---
+++ BOJ15652.java
@@ -0,0 +1,39 @@
+package com.github.naruseon.beakjoon;
+
+import java.util.*;
+
+// N°ú  M, silver 3
+// TODO
+
+public class BOJ15652 {
+	
+	static boolean[] isused;
+	static int N, M;
+	static int[] selected;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		isused = new boolean[N];
+		selected = new int[M];
+		solve(0, 0);
+	}
+	
+	public static void solve(int start, int k) {
+		if (k == M) {
+			for (int i : selected) {
+				System.out.print((i + 1) + " ");
+			}
+			System.out.println();
+			return;
+		}
+		for (int i = start; i < N; i++) {
+			if (!isused[i]) {
+				selected[k] = i;
+				solve(i, k + 1);
+				isused[i] = false;
+			}
+		}
+	}
+}
